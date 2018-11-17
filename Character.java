14da175eb384f2a0856cde692c8f07dd29095258
ManishGangal <manish0338@gmail.com>
2018-11-17 05:44:50 -0800

    =2nd

--- Character.java
+++ Character.java
@@ -14,6 +14,26 @@ public class Character extends Actor
      * Act - do whatever the Character wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    String serverName;
+    int port;
+    Socket client;
+    OutputStream outToServer;
+    ObjectOutputStream os;
+    XY xy;
+    
+    public Character(){
+        try{
+            serverName = "10.0.0.74";
+            port = Integer.parseInt("5000");
+            client = new Socket(serverName, port);
+            
+            outToServer = client.getOutputStream();
+            os = new ObjectOutputStream(outToServer);
+            xy = new XY();
+        }catch(Exception e){}
+    }
+    
     public void act() 
     {
         move();
@@ -30,17 +50,12 @@ public class Character extends Actor
         
         setLocation(x,y);
         try {
-            String serverName = "10.0.0.74";
-            int port = Integer.parseInt("5000");
-            Socket client = new Socket(serverName, port);
+
             
-            OutputStream outToServer = client.getOutputStream();
-            ObjectOutputStream os = new ObjectOutputStream(outToServer);
-            XY xy = new XY();
             xy.x = x;
             xy.y = y;
             os.writeObject(xy);
-            client.close();
+            
         }catch(Exception e){}
         
         
