c735a435874fde5636a720ae503c5da1ef0d8c1a
manish0338 <manish0338@gmail.com>
2018-11-21 19:31:01 -0800

    few bug fixes

--- client/FireCommand.java
+++ client/FireCommand.java
@@ -13,7 +13,10 @@ public class FireCommand implements Command {
        int y = this.actor.getY();
        Bullets bullet= new Bullets();
        FirePacket.bulletCounter++;
-       Sender.sendData(new FirePacket(x,y,1));
+       if(Acceptor.xy.id==0)
+        Sender.sendData(new FirePacket(x,y,1));
+       else
+        Sender.sendData(new FirePacket(x,y,-1));
        this.actor.getWorld().addObject(bullet,x,y);
        bullet.move(10);
     }
