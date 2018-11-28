5464a85a8afde9e57a31f1706afd1863f6382b19
Manish Gangal <manish0338@gmail.com>
2018-11-27 16:12:48 -0800

    changes in receiver

--- server/ReceiveUDP.java
+++ server/ReceiveUDP.java
@@ -6,6 +6,8 @@ import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +17,8 @@ public class ReceiveUDP extends Thread {
 	private DatagramSocket socket;
 	private DatagramSocket socket1;
 
-	List<SocketAddress> ipList = new ArrayList<SocketAddress>();
+	List<SocketAddress> soAddressList = new ArrayList<SocketAddress>();
+	List<InetAddress> ipList = new ArrayList<InetAddress>();
 
 	public ReceiveUDP() throws Exception {
 		socket = new DatagramSocket(5000);
@@ -27,33 +30,40 @@ public class ReceiveUDP extends Thread {
 		byte[] serializedMessage = new byte[1024];
 		while (true) {
 			try {
-
+				System.out.print( " = ");
 				DatagramPacket packet = new DatagramPacket(serializedMessage, serializedMessage.length);
+				
 				socket.receive(packet);
 
 				ObjectInputStream iStream = new ObjectInputStream(new ByteArrayInputStream(packet.getData()));
 				Object o = iStream.readObject();
 				iStream.close();
 				
-				
-				if (o instanceof DummyPacket && !ipList.contains(packet.getSocketAddress())) {
-					ipList.add(packet.getSocketAddress());
+				if (o instanceof DummyPacket ) {
+					System.out.print( " + ");
+					if(!soAddressList.contains(packet.getSocketAddress())) {
+						soAddressList.add(packet.getSocketAddress());
+						ipList.add(packet.getAddress());
+					}
+					
+					continue;
 				}
 
-				SocketAddress ip = packet.getSocketAddress();
-				//System.out.println("IP:"+packet.getAddress() + "  PORT:"+packet.getPort());
-
+				InetAddress ip = packet.getAddress();
+				SocketAddress so = null;
 				
+				System.out.println(soAddressList);
+				System.out.println(ipList.indexOf(ip) + "  index");
 				
 				try {
 					if (ipList.indexOf(ip) % 2 == 0)
-						ip = ipList.get(ipList.indexOf(ip) + 1);
+						so = soAddressList.get(ipList.indexOf(ip) + 1);
 					else
-						ip = ipList.get(ipList.indexOf(ip) - 1);
+						so = soAddressList.get(ipList.indexOf(ip) - 1);
 
 					byte[] newSerializedMessage = null;
 					if (o instanceof XY)
-						newSerializedMessage = getXY((XY) o, ip);
+						newSerializedMessage = getXY((XY) o, so);
 					else if (o instanceof FirePacket)
 						newSerializedMessage = getFirePacket((FirePacket) o);
 
@@ -61,10 +71,11 @@ public class ReceiveUDP extends Thread {
 					System.out.print(ip + " = ");
 					System.out.println(packet.getAddress());
 
-					packet.setSocketAddress(ip);
+					packet.setSocketAddress(so);
 
 					socket1.send(packet);
 				} catch (IndexOutOfBoundsException e) {
+					e.printStackTrace();
 				}
 
 			} catch (Exception e) {
@@ -77,8 +88,8 @@ public class ReceiveUDP extends Thread {
 	private byte[] getXY(XY xy, SocketAddress ip) throws IOException {
 		ByteArrayOutputStream bStream1 = new ByteArrayOutputStream();
 		ObjectOutput newoo = new ObjectOutputStream(bStream1);
-		xy.id = ipList.indexOf(ip);
-		System.out.println(xy.x + " - " + xy.y);
+		xy.id = soAddressList.indexOf(ip);
+		System.out.println(xy.x + " - " + xy.y+"  id: "+xy.id);
 		newoo.writeObject(xy);
 		newoo.close();
 		byte[] newSerializedMessage = bStream1.toByteArray();
